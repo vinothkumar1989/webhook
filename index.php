c7e7d0aca780107dea9a55e050560de621c5f046
vinothkumar1989 <48446047+vinothkumar1989@users.noreply.github.com>
2024-07-22 18:20:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,49 +28,5 @@ if (empty($data)) {
     exit;
 }
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "onlinepayment";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO onlinepayments (`name`, `email`, `phone`, `orderTime`, `orderId`, `amount`, `gst`, `discount`, `coupon`, `country`, `amountPayable`, `mangoName`, `status`, `slotId`, `eventId`, `slotDate`, `subscriberId`, `payment_id`)
-VALUES (
-    '" . mysqli_real_escape_string($conn, $data['name']) . "',
-    '" . mysqli_real_escape_string($conn, $data['email']) . "',
-    '" . mysqli_real_escape_string($conn, $data['phone']) . "',
-    '" . mysqli_real_escape_string($conn, $data['orderTime']) . "',
-    '" . mysqli_real_escape_string($conn, $data['orderId']) . "',
-    '" . mysqli_real_escape_string($conn, $data['amount']) . "',
-    '" . mysqli_real_escape_string($conn, $data['gst']) . "',
-    '" . mysqli_real_escape_string($conn, $data['discount']) . "',
-    '" . mysqli_real_escape_string($conn, $data['coupon']) . "',
-    '" . mysqli_real_escape_string($conn, $data['country']) . "',
-    '" . mysqli_real_escape_string($conn, $data['amountPayable']) . "',
-    '" . mysqli_real_escape_string($conn, $data['mangoName']) . "',
-    '" . mysqli_real_escape_string($conn, $data['status']) . "',
-    '" . mysqli_real_escape_string($conn, $data['slotId']) . "',
-    '" . mysqli_real_escape_string($conn, $data['eventId']) . "',
-    '" . mysqli_real_escape_string($conn, $data['slotDate']) . "',
-    '" . mysqli_real_escape_string($conn, $data['subscriberId']) . "',
-    '" . mysqli_real_escape_string($conn, $data['id']) . "'
-)";
-
-if (mysqli_query($conn, $sql)) {
-    // Log the request for debugging (optional)
-file_put_contents('webhook1.log', date('Y-m-d H:i:s') . " - Raw payload: New record created successfully");
-} else {
-    file_put_contents('webhook1.log', date('Y-m-d H:i:s') . " - Raw payload: Error creating New record");
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-
 
 ?>
